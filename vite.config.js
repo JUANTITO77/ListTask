df34552aae73cfcf00a2b2ba8fd9bb33a33e17db
JUANTITO77 <jelvistq77@gmail.com>
2024-05-23 17:19:58 -0500

    ya me canse de hacer cambios

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/juantito77/',
-})
+  base: '/ListTask/', // Asegúrate de que esta ruta es correcta
+});
