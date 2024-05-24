2a53d5d1f9ae8def7a7ec655a1ff61903494a474
JUANTITO77 <jelvistq77@gmail.com>
2024-05-23 19:52:57 -0500

    subiendo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ListTask/', // Asegúrate de que esta ruta es correcta
+  base: "https://juantito77.github.io/ListTask", // Asegúrate de que esta ruta es correcta
 });
